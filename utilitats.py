88ced761b917b816afe9f97baf7a309eae760a65
Nyx <arbarcel@gmail.com>
2026-06-08 23:43:15 +0200

    refactor: obertura de fitxer amb with open() a utilitats.py

--- utilitats.py
+++ utilitats.py
@@ -1,7 +1,6 @@
 # utilitats.py
 
 def escriure_linia(nom_fitxer, contingut):
-    f = open(nom_fitxer, "a", encoding="utf-8")
-    f.write(contingut + "\n")
-    f.close()
+    with open(nom_fitxer, "a", encoding="utf-8") as fitxer:
+        fitxer.write(contingut + "\n")
     return True
