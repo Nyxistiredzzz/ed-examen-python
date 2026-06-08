8a7907e38d8186025195274661339922d9d63404
Nyx <arbarcel@gmail.com>
2026-06-08 23:42:08 +0200

    feat: afegida gestió d'excepcions IndexError i FileNotFoundError

--- main.py
+++ main.py
@@ -2,18 +2,27 @@
 import sys
 from utilitats import escriure_linia
 
-nom_fitxer = sys.argv[1]
+try:
+    nom_fitxer = sys.argv[1]
+except IndexError:
+    print("Error: Has d'especificar el nom del fitxer per consola.")
+    sys.exit(1)
 
 print(f"--- Iniciant l'enregistrament al fitxer: {nom_fitxer} ---")
 print("Escriu text línia per línia. Per acabar, escriu 'final'.")
 
-bucle_actiu = True
-while bucle_actiu:
-    paraula_usuari = input("> ")
+try:
+    bucle_actiu = True
+    while bucle_actiu:
+        paraula_usuari = input("> ")
 
-    if paraula_usuari == "final":
-        bucle_actiu = False
-    else:
-        resultat = escriure_linia(nom_fitxer, paraula_usuari)
+        if paraula_usuari == "final":
+            bucle_actiu = False
+        else:
+            resultat = escriure_linia(nom_fitxer, paraula_usuari)
+
+except (FileNotFoundError, PermissionError) as error:
+    print(f"Error en accedir al fitxer: {error}")
+    sys.exit(1)
 
 print("Programa finalitzat correctament.")
